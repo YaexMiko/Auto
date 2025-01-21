052541fee0099cd202fd01e5478efc7805edb3af
YaexMiko <aloksinghmains756@gmail.com>
2025-01-21 20:53:36 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -39,7 +39,7 @@ class Var:
         LOGS.critical('Important Variables Missing. Fill Up and Retry..!! Exiting Now...')
         exit(1)
 
-    RSS_ITEMS = getenv("RSS_ITEMS", "https://nyaa.si/?page=rss&u=varyg1001").split()
+    RSS_ITEMS = getenv("RSS_ITEMS", "https://nyaa.si/?page=rss&u=subsplease").split()
     FSUB_CHATS = list(map(int, getenv('FSUB_CHATS').split()))
     BACKUP_CHANNEL = getenv("BACKUP_CHANNEL", "") or ""
     MAIN_CHANNEL = int(getenv("MAIN_CHANNEL", "-1002041917624"))
